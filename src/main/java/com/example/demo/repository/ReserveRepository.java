454f0ec4fe166be9a8195b4759e3439e5ebd406c
ItokuboOnegaikamisama <581-student051@la-study.com>
2025-06-12 15:00:41 +0900

    ReserveRepository.javaを作成しました。

--- src/main/java/com/example/demo/repository/ReserveRepository.java
+++ src/main/java/com/example/demo/repository/ReserveRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Reserve;
 
 public interface ReserveRepository extends JpaRepository<Reserve, Integer> {
-
+	List<Reserve> findByUserIdAndPlanIdAndQuantityAndRoomCount(Integer userId, Integer planId, Integer quantity,
+			Integer roomCount);
 }
