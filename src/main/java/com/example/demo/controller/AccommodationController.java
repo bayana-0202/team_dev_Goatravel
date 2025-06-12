d3322545fe80302dc328e0c16cdd085501e0f3bb
reskillKatsura <581-student015@la-study.com>
2025-06-12 17:19:33 +0900

    検索画面できました

--- src/main/java/com/example/demo/controller/AccommodationController.java
+++ src/main/java/com/example/demo/controller/AccommodationController.java
@@ -1,5 +1,23 @@
 package com.example.demo.controller;
 
-public class AccommodationController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.repository.AccommodationRepository;
+
+@Controller
+public class AccommodationController {
+	@Autowired
+	AccommodationRepository accommodationRepository;
+
+	@GetMapping("/")
+	public String index() {
+		return "searchView";
+	}
+
+	@GetMapping("/search")
+	public String searchListView() {
+		return "searchListView";
+	}
 }
