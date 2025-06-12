81b40ca95c1d6224ab3cd0b8235cb1176c76e96e
bayana-0202 <581-student049@la-study.com>
2025-06-12 16:26:51 +0900

    AccountAdminで管理者のログイン画面を表示

--- src/main/java/com/example/demo/controller/admin/AccountAdminController.java
+++ src/main/java/com/example/demo/controller/admin/AccountAdminController.java
@@ -1,5 +1,25 @@
 package com.example.demo.controller.admin;
 
-public class AccountAdminController {
+import jakarta.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.model.AccountAdmin;
+
+@Controller
+public class AccountAdminController {
+	@Autowired
+	HttpSession session;
+
+	@Autowired
+	AccountAdmin accountAdmin;
+
+	//管理者のログイン画面を表示
+	@GetMapping({ "/admin/login", "/admin/logout" })
+	public String index() {
+
+		return "admin/adminLogin";
+	}
 }
