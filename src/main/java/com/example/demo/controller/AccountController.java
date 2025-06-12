1c0f77285a75bde83b85734556006e0980acf39a
kakuda2025 <581-student013@la-study.com>
2025-06-12 16:54:13 +0900

    アカウント作成1回目

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,5 +1,57 @@
 package com.example.demo.controller;
 
-public class AccountController {
+import java.util.List;
 
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.User;
+import com.example.demo.model.Account;
+import com.example.demo.repository.UserRepository;
+
+@Controller
+public class AccountController {
+	@Autowired
+	HttpSession session;
+
+	@Autowired
+	Account account;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping({ "/login" })
+	public String index(Model model) {
+		session.invalidate();
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String login(
+			@RequestParam("email") String email,
+			@RequestParam("password") String password,
+			Model model) {
+		List<User> users = userRepository.findByEmailAndPassword(email, password);
+		if (users.size() == 0) {
+			model.addAttribute("message", "メールアドレスまたはパスワードが違います");
+			return "login";
+		} else if (users.size() == 1) {
+			account.setId(users.get(0).getId());
+			account.setName(users.get(0).getName());
+			account.setTel(users.get(0).getTel());
+			account.setAddress(users.get(0).getAddress());
+			account.setPassword(users.get(0).getPassword());
+			account.setGender(users.get(0).getGender());
+			account.setNickname(users.get(0).getNickname());
+			account.setEmail(users.get(0).getEmail());
+			account.setBirthday(users.get(0).getBirthday());
+		}
+		return "redirect:/search";
+	}
 }
--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,13 +1,30 @@
 package com.example.demo.model;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
 @Component
 @SessionScope
 public class Account {
-
+	private Integer id;
 	private String name;
+	private String tel;
+	private String address;
+	private String password;
+	private Integer gender;
+	private String nickname;
+	private String email;
+	private LocalDate birthday;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
@@ -17,4 +34,59 @@ public class Account {
 		this.name = name;
 	}
 
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
 }
