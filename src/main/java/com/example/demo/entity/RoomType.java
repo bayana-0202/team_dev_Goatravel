08a4bdb1a3ce5d2565a0d750aa0c96e6cc3facf2
imai-2025-03 <581-student046@la-study.com>
2025-06-12 14:40:29 +0900

    RoomTypeのEntityを作りました

--- src/main/java/com/example/demo/entity/RoomType.java
+++ src/main/java/com/example/demo/entity/RoomType.java
@@ -1,5 +1,40 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "room_types")
 public class RoomType {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; //部屋ID
+	private String name; //部屋の種類
+
+	//ゲッター
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	//セッター
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//コンストラクタ
+	public RoomType() {
+
+	}
+
+	public RoomType(String name) {
+		this.name = name;
+	}
 
 }
