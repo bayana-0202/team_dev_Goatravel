4701b4cab307bd6a927574a36dff6666840995c3
shigeshigedesu2025 <581-student023@la-study.com>
2025-06-12 15:10:44 +0900

    planのRepositoryです

--- src/main/java/com/example/demo/repository/PlanRepository.java
+++ src/main/java/com/example/demo/repository/PlanRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Plan;
 
 public interface PlanRepository extends JpaRepository<Plan, Integer> {
+
+	List<Plan> findByRoomIdAndPriceAndAccommodationIdAndDateBetween(Integer roomId, Integer price,
+			Integer accommodationId, LocalDate startDate, LocalDate endDate);
 
 }
