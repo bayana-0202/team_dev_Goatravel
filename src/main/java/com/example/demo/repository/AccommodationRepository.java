167174e9a5aa740db53f521294474c1cc6798e54
imai-2025-03 <581-student046@la-study.com>
2025-06-12 14:58:13 +0900

    AccommodationRepositoryを作りました

--- src/main/java/com/example/demo/repository/AccommodationRepository.java
+++ src/main/java/com/example/demo/repository/AccommodationRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Accommodation;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation, Integer> {
+
+	//宿泊施設のカテゴリー、対応言語
+	List<Accommodation> findByCategoryIdAndLanguageId(Integer categoryId, Integer languageId);
 
 }
