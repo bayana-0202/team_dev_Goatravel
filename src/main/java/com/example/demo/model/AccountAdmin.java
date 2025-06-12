bd15f32031eedd98e3a7fff5a01e98e56bf81421
bayana-0202 <581-student049@la-study.com>
2025-06-12 17:07:38 +0900

    Repositoryの内容を追加しました

--- src/main/java/com/example/demo/model/AccountAdmin.java
+++ src/main/java/com/example/demo/model/AccountAdmin.java
@@ -16,4 +16,15 @@ public class AccountAdmin {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	private Integer id;
+
 }
