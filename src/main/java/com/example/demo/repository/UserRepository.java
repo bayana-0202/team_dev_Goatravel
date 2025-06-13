171ad910ebe7fd36d3f7a4e53faf1a6a5f7dc087
shigeshigedesu2025 <581-student023@la-study.com>
2025-06-13 15:27:07 +0900

    新規登録から完了まで一通り終わりました

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,8 @@ import com.example.demo.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	List<User> findByEmailAndPassword(String email, String password);
+
+	Optional<User> findByEmail(String email);
+
+	Optional<User> findByTel(String tel);
 }
