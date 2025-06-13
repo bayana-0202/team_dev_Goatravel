93855a3d1bff0aed76367abd486557e1dffad8ef
imai-2025-03 <581-student046@la-study.com>
2025-06-13 14:27:58 +0900

    削除機能作成途中

--- src/main/java/com/example/demo/controller/admin/AccommodationAdminController.java
+++ src/main/java/com/example/demo/controller/admin/AccommodationAdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -99,5 +100,12 @@ public class AccommodationAdminController {
 		model.addAttribute("users", userList);
 
 		return "admin/adminReserve";
+	}
+
+	//予約を削除する
+	@PostMapping("/admin/{id}/delete")
+	public String deleteReserved(@PathVariable("id") Integer id) {
+		reserveRepository.deleteById(id);
+		return "redirect:/admin/accommodation";
 	}
 }
